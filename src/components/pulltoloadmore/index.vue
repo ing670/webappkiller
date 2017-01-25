4f185936f27f862e6e56ba512bbc4aabe05729ef
nG <ing670@163.com>
2017-01-25 10:30:00 +0800

    src/components/pulltoloadmore/index.vue

--- src/components/pulltoloadmore/index.vue
+++ src/components/pulltoloadmore/index.vue
@@ -2,10 +2,10 @@
     <div ref="pulltoloadmorewraper" class="wk-pulltoloadmore">
         <slot name="pulldown">
 
-            <div ref="pulldown" class="wk-pulldown" v-show="true">
-                <icon class="pulldown_arrow" ref="pulldown_arrow" fontCode="e5db"></icon>
-                <!--<circular v-show="true" :size="24" color="carbon"></circular>-->
-                <span v-show="pullDownLoading" class="wk-infinite-scroll-text">正在加载。。。</span>
+            <div ref="pulldown" class="wk-pulldown">
+                <icon v-show="!pullDownLoading" class="pulldown_arrow" ref="pulldown_arrow" fontCode="e5db"></icon>
+                <circular v-show="pullDownLoading" :size="24" color="carbon"></circular>
+                <span class="wk-pulldown-scroll-text">正在加载。。。</span>
             </div>
         </slot>
         <slot>
@@ -26,29 +26,31 @@
         </slot>
 
         <slot name="pullup">
-            <div ref="pullup" class="wk-pullup" style="display: block">
-                <circular style="display: block" :size="24" color="carbon"></circular>
-                <span v-show="loading" class="wk-infinite-scroll-text">正在加载。。。</span>
+            <div ref="pullup" class="wk-pullup" >
+                <icon v-show="!pullUpLoading" class="pullup_arrow" ref="pullup_arrow" fontCode="e5d8"></icon>
+                <circular v-show="pullUpLoading" :size="24" color="carbon"></circular>
+                <span class="wk-pulldown-scroll-text">正在加载。。。</span>
             </div>
         </slot>
 
     </div>
 </template>
 <script>
-//    e5db
+    //    e5db
     import circular from '../circular'
     import icon from '../icon'
 
     export default {
         data(){
             return {
-                loading: false,
+                pullUpLoading: false,
                 pullDownLoading: false,
                 startY: 0,
                 endY: 0,
                 pulldownHeight: 0,
                 pullupHeight: 0,
-                contentOffsetTop: 0
+                contentOffsetTop: 0,
+                contentOffsetBottom:0
             }
         },
         methods: {},
@@ -59,38 +61,60 @@
             this.$refs.pulldown.style.top = (0 - this.pulldownHeight) + 'px';
             this.$refs.pullup.style.bottom = (0 - this.pullupHeight) + 'px';
             this.$refs.pullcontent.addEventListener('touchstart', (e) => {
-                //this.startY = e.touches[0].clientY;
-                //
-                if (this.pullDownLoading == false && this.$refs.pullcontent.scrollTop == 0) {
+                let isBottom=this.$refs.pullcontent.scrollHeight-this.$refs.pullcontent.scrollTop==this.$refs.pullcontent.clientHeight?true:false;
+
+                if ((this.pullDownLoading == false && this.$refs.pullcontent.scrollTop == 0) || isBottom) {
                     this.startY = e.touches[0].clientY;
                 }
             }, false);
             this.$refs.pullcontent.addEventListener('touchmove', (e) => {
                 let distance = (e.touches[0].clientY - this.startY);
-
+                //下拉效果
                 if (this.pullDownLoading == false && this.$refs.pullcontent.scrollTop == 0 && distance > 0) {
                     e.preventDefault();
                     e.stopPropagation();
-                    this.endY = e.touches[0].clientY;
                     this.contentOffsetTop = distance / 2;
-                    this.$refs.pulldown.style.transform = 'translate3d(0,' + distance / 5 + 'px,0)';
+                    this.$refs.pulldown.style.transform = 'translate3d(0,' + distance / 3 + 'px,0)';
                     this.$refs.pullcontent.style.transform = 'translate3d(0,' + distance / 2 + 'px,0)';
-                    if(distance/2<=180){
-                        this.$refs.pulldown_arrow.$el.style.transform='rotate('+(distance/2 )+'deg)'
-
+                    if (distance / 2 <= 180) {
+                        this.$refs.pulldown_arrow.$el.style.transform = 'rotate(' + (distance / 2 ) + 'deg)'
                     }
+                    //控制箭头
+                    if (this.contentOffsetTop / 2 > this.pulldownHeight) {
+                        this.$refs.pulldown_arrow.$el.style.transform = 'rotate(180deg)'
+                    } else {
+                        this.$refs.pulldown_arrow.$el.style.transform = 'rotate(0deg)'
+                    }
+                }
+                //上拉效果
+                let isBottom=this.$refs.pullcontent.scrollHeight-this.$refs.pullcontent.scrollTop==this.$refs.pullcontent.clientHeight?true:false;
 
+                if (this.pullUpLoading == false && isBottom && distance < 0) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    this.contentOffsetBottom = distance / 2;
+                    this.$refs.pullup.style.transform = 'translate3d(0,' + distance / 3 + 'px,0)';
+                    this.$refs.pullcontent.style.transform = 'translate3d(0,' + distance / 2 + 'px,0)';
+                    if (distance / 2 <= 180) {
+                        this.$refs.pullup_arrow.$el.style.transform = 'rotate(' + (distance / 2 ) + 'deg)'
+                    }
+                    //控制箭头
+                    if (this.contentOffsetBottom / 2 > -this.pulldownHeight) {
+                        this.$refs.pullup_arrow.$el.style.transform = 'rotate(180deg)'
+                    } else {
+                        this.$refs.pullup_arrow.$el.style.transform = 'rotate(0deg)'
+                    }
                 }
 
             }, false);
             this.$refs.pullcontent.addEventListener('touchend', (e) => {
-                //
+                //下拉刷新
                 if (this.pullDownLoading == false && this.$refs.pullcontent.scrollTop == 0) {
 
                     if (this.contentOffsetTop > this.pulldownHeight) {
                         this.$refs.pullcontent.style.transform = 'translate3d(0,' + this.pulldownHeight + 'px,0)';
                         this.$refs.pulldown.style.transform = 'translate3d(0,' + this.pulldownHeight + 'px,0)';
-                        this.$refs.pulldown_arrow.$el.style.transform='rotate(180deg)'
+                        this.$refs.pulldown_arrow.$el.style.transform = 'rotate(180deg)'
 
                         this.pullDownLoading = true;
                         setTimeout(() => {
@@ -98,20 +122,49 @@
                             this.contentOffsetTop = 0;
                             this.$refs.pullcontent.style.transform = 'translate3d(0,0px,0)';
                             this.$refs.pulldown.style.transform = 'translate3d(0,0px,0)';
-                            this.$refs.pulldown_arrow.$el.style.transform='rotate(0deg)'
-
-                            setTimeout(() => this.pullDownLoading = false, 200)
+                            setTimeout(() => {
+                                this.pullDownLoading = false;
+                                this.$refs.pulldown_arrow.$el.style.transform = 'rotate(0deg)'
+                            }, 200);
                         }, 2000);
                     } else {
                         this.$refs.pullcontent.style.transform = 'translate3d(0,0,0)';
                         this.$refs.pulldown.style.transform = 'translate3d(0,0,0)';
+                        this.$refs.pulldown_arrow.$el.style.transform = 'rotate(0deg)'
                     }
                 }
+                let isBottom=this.$refs.pullcontent.scrollHeight-this.$refs.pullcontent.scrollTop==this.$refs.pullcontent.clientHeight?true:false;
+
+                if (this.pullUpLoading == false && isBottom) {
+
+                    if ((0-this.contentOffsetBottom) > this.pullupHeight) {
+                        this.$refs.pullcontent.style.transform = 'translate3d(0,' + -this.pullupHeight + 'px,0)';
+                        this.$refs.pullup.style.transform = 'translate3d(0,' + -this.pullupHeight + 'px,0)';
+                        this.$refs.pullup_arrow.$el.style.transform = 'rotate(180deg)'
+
+                        this.pullUpLoading = true;
+                        setTimeout(() => {
+                            this.startY = 0;
+                            this.contentOffsetBottom = 0;
+                            this.$refs.pullcontent.style.transform = 'translate3d(0,0px,0)';
+                            this.$refs.pullup.style.transform = 'translate3d(0,0px,0)';
+                            setTimeout(() => {
+                                this.pullUpLoading = false;
+                                this.$refs.pullup_arrow.$el.style.transform = 'rotate(0deg)'
+                            }, 200);
+                        }, 2000);
+                    } else {
+                        this.$refs.pullcontent.style.transform = 'translate3d(0,0,0)';
+                        this.$refs.pullup.style.transform = 'translate3d(0,0,0)';
+                        this.$refs.pullup_arrow.$el.style.transform = 'rotate(0deg)'
+                    }
+                }
+
             }, false);
 
         },
         components: {
-            circular,icon
+            circular, icon
         }
     }
 </script>
@@ -125,19 +178,31 @@
         .wk-pullup {
             position: absolute;
             width: 100%;
-            font-size: 32px;
+            font-size: .64rem
 
         }
-        .pulldown_arrow{
-            transition: all .2s ease;
-            font-size: .64rem;
+        .pulldown_arrow ,.pullup_arrow{
+            transition: all .3s ease;
+            font-size: .36rem;
+            line-height: .64rem;
+
         }
-        .wk-pulldown {
+        .wk-pulldown,.wk-pullup {
             position: absolute;
             width: 100%;
-            font-size: 32px;
+            font-size: .64rem;
             transition: all .2s ease;
+            display: flex;
+            justify-content: center;
+            align-content: center;
         }
+    }
+
+    .wk-pulldown-scroll-text {
+        display: block;
+        margin-left: .16rem;
+        font-size: .16rem;
+        line-height: .64rem;
     }
 
     .wk-pull-content {
